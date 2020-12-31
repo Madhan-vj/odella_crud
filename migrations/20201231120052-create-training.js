d0b6fb284c4af585a8a72ff3b6f2b5cbc6ca4e8e
Madhan-vj <59563681+Madhan-vj@users.noreply.github.com>
2020-12-31 17:35:46 +0530

    TrainingModel

--- migrations/20201231120052-create-training.js
+++ migrations/20201231120052-create-training.js
@@ -11,6 +11,9 @@ module.exports = {
       name: {
         type: Sequelize.STRING
       },
+      reference: {
+        type: Sequelize.STRING
+      },
       moduleId: {
         type: Sequelize.INTEGER,
         allowNull: false,
@@ -26,6 +29,33 @@ module.exports = {
           model: 'Categories',
           key: 'id'
         }
+      },
+      customization: {
+        type: Sequelize.STRING
+      },
+      supportingDocumentUrl: {
+        type: Sequelize.STRING
+      },
+      supportingDocumentName: {
+        type: Sequelize.STRING
+      },
+      trainingDescription: {
+        type: Sequelize.STRING
+      },
+      trainingPrice: {
+        type: Sequelize.INTEGER
+      },
+      trainingTax: {
+        type: Sequelize.INTEGER
+      },
+      trainingOffer: {
+        type: Sequelize.INTEGER
+      },
+      trainingDiscount: {
+        type: Sequelize.INTEGER
+      },
+      MTP: {
+        type: Sequelize.BOOLEAN
       },
       createdAt: {
         allowNull: false,
--- models/training.js
+++ models/training.js
@@ -12,15 +12,25 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Training.belongsTo(models.Module, {foreignKey: 'moduleId', as: 'module'});
-      Training.belongsTo(models.Module, {foreignKey: 'categoryId', as: 'category'});
+      Training.belongsTo(models.Module, {foreignKey: 'categoryId', as: 'category'});  
       Training.hasMany(models.Location, {as: 'Location'});
       Training.belongsToMany(models.Tags, {through: 'TrainingTags', foreignKey: 'trainingId', as: 'tag'})
     }
   };
   Training.init({
     name: DataTypes.STRING,
+    reference: DataTypes.STRING,
     moduleId: DataTypes.INTEGER,
-    categoryId: DataTypes.INTEGER
+    categoryId: DataTypes.INTEGER,
+    customization: DataTypes.STRING,
+    supportingDocumentUrl: DataTypes.STRING,
+    supportingDocumentName: DataTypes.STRING,
+    trainingDescription: DataTypes.STRING,
+    trainingPrice: DataTypes.INTEGER,
+    trainingTax: DataTypes.INTEGER,
+    trainingOffer: DataTypes.INTEGER,
+    trainingDiscount: DataTypes.INTEGER,
+    MTP: DataTypes.BOOLEAN
   }, {
     sequelize,
     modelName: 'Training',
