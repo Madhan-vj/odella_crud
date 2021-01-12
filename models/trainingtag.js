687e3f3de1bc14cf1dac4e53f35379bc9ccbeaf3
kmkumar007 <muthukumar@gridsandguides.com>
2021-01-12 16:25:39 +0530

    training_tag_relation

--- models/trainingtag.js
+++ models/trainingtag.js
@@ -11,8 +11,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      TrainingTag.belongsTo(models.Training, {foreignKey: 'trainingId'})
-
+      TrainingTag.belongsTo(models.Training, {foreignKey: 'trainingId'});
+      TrainingTag.belongsTo(models.Tag, {foreignKey: 'tagId'})
     }
   };
   TrainingTag.init({
